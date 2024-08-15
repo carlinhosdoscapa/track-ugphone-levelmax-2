ee1eeabec77c6e88a971106587e208a5ae1d36ea
carlinhosdoscapa <carlinhosnomatagal208@gmail.com>
2024-08-15 15:48:51 -0300

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -48,5 +48,5 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitin
 
 -- AUTO TRACKER YUMMY
 
-_G.Config = { UserID = "2ecc7ebd-4788-47be-b07d-43dd6589ac94", discord_id = 1037147029614362624 , Note = "[LEVEL MAX] UGPHONE - 1", } 
+_G.Config = { UserID = "2ecc7ebd-4788-47be-b07d-43dd6589ac94", discord_id = 1037147029614362624 , Note = "[LEVEL MAX] UGPHONE - 2", } 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/main/trackstatblox"))()
