db99dfef3546cb7aec00fca46be7a9a6c1d38bb5
carlinhosdoscapa <carlinhosnomatagal208@gmail.com>
2024-08-15 15:30:51 -0300

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -8,7 +8,8 @@ repeat task.wait() until game.Players.LocalPlayer.PlayerGui:FindFirstChild("Main
 
 -- AUTO TRACKER YUMMY
 
-_G.Config = { UserID = "2ecc7ebd-4788-47be-b07d-43dd6589ac94", discord_id = 1037147029614362624 , Note = "[LEVEL MAX] UGPHONE - 1", } loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/main/trackstatblox"))()
+_G.Config = { UserID = "2ecc7ebd-4788-47be-b07d-43dd6589ac94", discord_id = 1037147029614362624 , Note = "[LEVEL MAX] UGPHONE - 1", } 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/main/trackstatblox"))()
 
 
 getgenv().Key = "608cd0870cd348122a1dc06b"
